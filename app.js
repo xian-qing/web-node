e827b00abfe54d4ed893c657ecb3d70f9651b926
wxq <stone@xintiaotime.com>
2017-05-09 20:27:43 +0800

    d

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 //express_demo.js 文件
 const express = require('express');
+const mysql  = require('mysql');  //调用MySQL模块
+const conf = require('./config');
 const app = express();
 const server = require('http').createServer(app);
 server.listen(5207);
-console.log('执行')
+var connection = mysql.createConnection(conf.sql);
+connection.connect(function(err){
+    if(err){
+        console.log('[query] - :'+err);
+        return;
+    }
+
+    console.log('[connection connect]  succeed!');
+
+});
+
+connection.query('select * from wxq_user', function(err, rows, fields) {
+
+    if (err) {
+
+        console.log('[query] - :'+err);
+
+        return;
+
+    }
+
+    console.log(rows);
+
+});
 
 // router
 app.use('/', require("./router/index.js"))
---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by stone on 2017/5/6.
+ */
+//配置
+module.exports = {
+    sql:{
+        host:'139.199.72.98',
+        user:'root',
+        password:'MjyxCvsmS7_F',
+        database: 'mysql',
+        charset:'UTF8'
+    }
+}
