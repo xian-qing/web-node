5cd4619eebaddbc90262ee1a0025bd9fcb15fdb1
wxq <stone@xintiaotime.com>
2017-05-09 20:41:22 +0800

    d

--- controller/test.js
+++ controller/test.js
@@ -4,4 +4,16 @@ module.exports  = {
         var results = {test:true}
         res.send(results)
     },
+    get(req, res){
+        var results = {
+            result:0,
+            data:[
+                {id:1,name:'撒旦法'},
+                {id:2,name:'的双方各'},
+                {id:3,name:'个'},
+            ],
+            msg:'成功'
+        }
+        res.send(results)
+    }
 };
--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
 const router = express.Router()
 router.get('/n', require('../controller/test.js').inter)
+router.get('/n/get', require('../controller/test.js').get)
 module.exports = router
